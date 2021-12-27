37f25aae011e0838fe7c51e1c25f5e08e6adba15
Kevin Hernandez <kevin_hdz97@hotmail.com>
2021-12-27 13:49:26 -0600

    Added

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code>. 4th edit modifying now after amplify.yml now modified basedir. Modified cd reactapp before build
+          Edit <code>src/App.js</code>. 4th edit modifying now after amplify.yml now modified basedir. Modified cd reactapp before build.
+          Changed further now to add a prebuild
         </p>
         <a
           className="App-link"
