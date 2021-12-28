92a4b72b6c0ca85027bac355e5f603465884281c
Kevin Hernandez <kevin_hdz97@hotmail.com>
2021-12-28 14:30:21 -0600

    Amplify push to create Cognito User Pool

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "tutorial1e76459b3": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
